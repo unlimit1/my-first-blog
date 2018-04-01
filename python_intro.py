b2a37e44b32f1f6a2bf399632dd7d445f597c1ba
lyh <yonghoon.lim@sk.com>
2018-04-01 20:01:54 +0900

    장고걸스 post 앱

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+print('Hello, Django girls!')
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
